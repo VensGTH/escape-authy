b8adc561605f92492c77916d39fb7b11ac7d4e3b
unknown <treplay009@gmail.com>
2025-07-25 00:24:51 +0200

    fixed aegis export format

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.4
 require (
 	github.com/google/uuid v1.6.0
 	github.com/spf13/cobra v1.9.1
+	golang.org/x/text v0.27.0
 )
 
 require (
--- model/aegis-entry.go
+++ model/aegis-entry.go
@@ -1,5 +1,21 @@
 package model
 
+type AegisBase struct {
+	Version int         `json:"version"`
+	Header  AegisHeader `json:"header"`
+	Db      AegisDB     `json:"db"`
+}
+
+type AegisHeader struct {
+	Slots  *string `json:"slots"`
+	Params *string `json:"params"`
+}
+
+type AegisDB struct {
+	Version int          `json:"version"`
+	Entries []AegisEntry `json:"entries"`
+}
+
 type AegisEntry struct {
 	Type   string    `json:"type"`
 	UUID   string    `json:"uuid"`
